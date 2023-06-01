11402e4e4e06496fc40c134e70d0ffa1607503b3
Brahim <brahim_info>
2023-06-01 19:17:22 +0100

    toast

--- app/src/main/java/com/example/myapplication/fg.java
+++ app/src/main/java/com/example/myapplication/fg.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class fg extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class fg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fg);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
